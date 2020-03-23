92ceb8df44d91abde7ac4f153c0b0599430393f5
Bob (andrey) <Bob@example.com>
2020-03-23 19:05:08 +0300

    Fixed min

--- main.cpp
+++ main.cpp
@@ -5,12 +5,20 @@ using namespace std;
 int main()
 {
     cout << "Enter A and B: ";
-    int a, b;
+    double a, b;
     cin >> a >> b;
     cout << "A + B = " << a + b << '\n';
     cout << "A - B = " << a - b << '\n';
     cout << "A * B = " << a * b << '\n';
     cout << "A / B = " << a / b << '\n';
+// HEAD
+// HEAD
+    cout << "Max " << max(a,b) << '\n';
+//////// HEAD
+    //fixed max
+// code:added min
+//
     cout << "min = " << min(a,b) << '\n';
 
+//// Fixed min
 }
